139809aa78922400a7d87688e9f29156784ad863
Vasily93 <markovv.dev@gmail.com>
2022-06-15 09:07:31 -0700

    keys update

--- src/helpers/emailHelpers.js
+++ src/helpers/emailHelpers.js
@@ -1,7 +1,7 @@
 import emailjs from '@emailjs/browser';
 
 const sendEmail = (obj) => {
-    emailjs.send('service_n66x87a', 'template_s8hb4od', obj, 't_ZimR3kvgZrIp5g3')
+    emailjs.send(serviceID, templateID, templateParams, publicKey)
   .then((result) => {
       console.log(result.text);
   }, (error) => {
