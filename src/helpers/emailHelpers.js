d4409f0f418a06e6f700845ec35e0f6c0508b7be
Vasily93 <markovavasily@gmail.com>
2022-06-17 15:45:44 -0700

    sending emails

--- src/helpers/emailHelpers.js
+++ src/helpers/emailHelpers.js
@@ -1,7 +1,11 @@
 import emailjs from '@emailjs/browser';
 
+const serviceID= process.env.REACT_APP_SERVICE_ID;
+const templateID= process.env.REACT_APP_TEMPLATE_ID;
+const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+console.log(serviceID, templateID, publicKey)
 const sendEmail = (obj) => {
-    emailjs.send(serviceID, templateID, templateParams, publicKey)
+    emailjs.send(serviceID, templateID, obj, publicKey)
   .then((result) => {
       console.log(result.text);
   }, (error) => {
