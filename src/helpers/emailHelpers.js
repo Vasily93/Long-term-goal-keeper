05a5195cc2d83c7d90717954678cd070b8337ccd
Vasily93 <markovv.dev@gmail.com>
2022-06-17 15:47:49 -0700

    removed console log

--- src/helpers/emailHelpers.js
+++ src/helpers/emailHelpers.js
@@ -3,7 +3,7 @@ import emailjs from '@emailjs/browser';
 const serviceID= process.env.REACT_APP_SERVICE_ID;
 const templateID= process.env.REACT_APP_TEMPLATE_ID;
 const publicKey = process.env.REACT_APP_PUBLIC_KEY;
-console.log(serviceID, templateID, publicKey)
+
 const sendEmail = (obj) => {
     emailjs.send(serviceID, templateID, obj, publicKey)
   .then((result) => {
